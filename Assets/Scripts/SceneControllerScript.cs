cae1342286371335e757127724e931f938f415fb
Gerdie <mariakmoy@gmail.com>
2017-04-02 16:06:20 -0700

    First pass at placing init scene objs

--- Assets/Scripts/SceneControllerScript.cs
+++ Assets/Scripts/SceneControllerScript.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SceneControllerScript : MonoBehaviour {
+	public const int gridRows = 3;
+	public const int gridCols = 3;
+	[SerializeField] private FarmTileBase farm_tile;
 
 	// Use this for initialization
 	void Start () {
-		
+		Vector3 startPos = farm_tile.transform.position;
+
+		for (int y=0; y < gridCols; y++) {
+			for (int x = 0; x < gridRows; x++) {
+				FarmTileBase tile;
+				if (x == 0 && y == 0) {
+					tile = farm_tile;
+				} else {
+					tile = Instantiate (farm_tile) as FarmTileBase;
+				}
+			}
+		{
 	}
 	
 	// Update is called once per frame
