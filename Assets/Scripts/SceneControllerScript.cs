7672588184a27181ffd2b0d5f6382e8def915eac
Gerdie <mariakmoy@gmail.com>
2017-05-11 22:07:45 -0400

    Static code tile start pos

--- Assets/Scripts/SceneControllerScript.cs
+++ Assets/Scripts/SceneControllerScript.cs
@@ -6,9 +6,20 @@ using UnityEngine;
 public class SceneControllerScript : MonoBehaviour {
 	public const int gridRows = 3;
 	public const int gridCols = 3;
+	private List <Vector3> gridPositions = new List<Vector3>();
 	[SerializeField] private FarmTileBase farm_tile;
 
 	// Use this for initialization
+	void SetScene () {
+		gridPositions.Add(new Vector3 (0f, 0f, 0f));
+		gridPositions.Add(new Vector3 (0.887, 0.747, 0f));
+		gridPositions.Add(new Vector3 (1.78, 1.49, 0f));
+		gridPositions.Add(new Vector3 (0.89, -0.736, 0f));
+		gridPositions.Add(new Vector3 (2.66, 0.747, 0f));
+		gridPositions.Add(new Vector3 (1.79, -1.47, 0f));
+		gridPositions.Add(new Vector3 (2.68, -0.72, 0f));
+		gridPositions.Add(new Vector3 (3.56, 0.02, 0f));
+	}
 	void Start () {
 		Vector3 startPos = farm_tile.transform.position;
 
