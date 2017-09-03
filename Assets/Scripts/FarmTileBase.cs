10561d06ae18cfa8676eae360095531e8d61b0c1
Gerdie <mariakmoy@gmail.com>
2017-09-02 17:58:51 -0700

    Add Plant base script

--- Assets/Scripts/FarmTileBase.cs
+++ Assets/Scripts/FarmTileBase.cs
@@ -1,28 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Plant;
 
 public class FarmTileBase : MonoBehaviour {
 	[SerializeField] private Sprite farm_tile;
 	[SerializeField] private Sprite soil_tile;
-
-//	public GameObject Hand;
-	public GameObject Hand = GameObject.FindGameObjectWithTag("Player");
-
-	public void OnMouseDown() {
-		int inHand = Hand.GetComponent<HandScript>().ToolInHand;
-		if (inHand == 1) { // 0 means nothing
-			GetComponent<SpriteRenderer> ().sprite = soil_tile;
-		} 
-	}
+	private bool is_hoed;
+	private bool is_planted;
+	private Plant plant;
+	private int days_growing;
 
 	// Use this for initialization
 	void Start () {
 		GetComponent<SpriteRenderer> ().sprite = farm_tile;
+		is_hoed = false;
+		is_planted = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+	public void Hoe() {
+		is_hoed = true;
+		GetComponent<SpriteRenderer> ().sprite = soil_tile;
+	}
+
+	public void Plant(object Plant) {
+		if (is_hoed && !is_planted) {
+			is_planted = true;
+			plant = Plant;
+			days_growing = 0;
+		}
+	}
+
+
 }
---
+++ Assets/Scripts/Plant.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant {
+
+	// Plant attributes
+	public int crop_id;
+	public int max_crop_age;
+	public string crop_name;
+
+	// TODO: add mapping between days_growing and appropriate sprite
+
+}
